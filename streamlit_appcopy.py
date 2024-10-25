79765f85da43d5217e26e6bdfe7978f74b757209
adrianbenitezrueda <abenitezdesign@gmail.com>
2024-10-25 08:10:09 +0200

    Update streamlit_appcopy.py

--- streamlit_appcopy.py
+++ streamlit_appcopy.py
@@ -126,7 +126,7 @@ def display_task_card(task):
 
 def main():
     # Header con estilo
-    st.markdown('#🎯 Bored no more: ¡Encuentra algo divertido que hacer!')
+    st.markdown('# 🎯 Bored no more: ¡Encuentra algo divertido que hacer!')
     
     # Sidebar con información del tiempo y ubicación
     with st.sidebar:
